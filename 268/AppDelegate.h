65c41ed04cece877cbafa70fa75f8fe370c3da94
eaph sing <xing.yf@yougou.com>
2018-01-08 10:21:08 +0800

    Initial Commit

---
+++ 268/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  268
+//
+//  Created by Eaph Sing on 2018/1/8.
+//  Copyright © 2018年 Ray Wenderlich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
